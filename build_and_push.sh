fc0d900a236e14d9327460a510cabcfd80affc09
Miguel Antoons <miguel.antoons@gmail.com>
2025-12-26 11:24:58 +0100

    Adding Dockerfile for mongodb and adding it to build_and_push script

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,5 +2,7 @@
 npm run build --prefix frontend/
 docker build -t 192.168.1.155:5000/facturation_frontend:latest -f frontend/docker/Dockerfile .
 docker build -t 192.168.1.155:5000/facturation_backend:latest -f backend/docker/Dockerfile .
+docker build -t 192.168.1.155:5000/facturation_database:latest -f database/docker/Dockerfile .
 docker push 192.168.1.155:5000/facturation_frontend:latest
 docker push 192.168.1.155:5000/facturation_backend:latest
+docker push 192.168.1.155:5000/facturation_database:latest
