1528a9fc5f97dc61bfe07db4c1fe5305d465a765
Miguel Antoons <miguel.antoons@gmail.com>
2026-01-03 21:57:37 +0100

    FEATURE: Modifying build_and_push script to only push when on main branch

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+echo "Building frontend..."
 npm run build --prefix frontend/
+echo "Updating backend requirements..."
 backend/venv/bin/python -m pip freeze > backend/requirements.txt
+echo "Building docker images..."
 docker build -t 192.168.1.155:5000/facturation_frontend:latest -f frontend/docker/Dockerfile .
 docker build -t 192.168.1.155:5000/facturation_backend:latest -f backend/docker/Dockerfile .
 docker build -t 192.168.1.155:5000/facturation_database:latest -f database/docker/Dockerfile .
-docker push 192.168.1.155:5000/facturation_frontend:latest
-docker push 192.168.1.155:5000/facturation_backend:latest
-docker push 192.168.1.155:5000/facturation_database:latest
+if [ "$(git branch --show-current)" = "main" ]; then
+    echo "On main branch, pushing docker images..."
+    docker push 192.168.1.155:5000/facturation_frontend:latest
+    docker push 192.168.1.155:5000/facturation_backend:latest
+    docker push 192.168.1.155:5000/facturation_database:latest
+else
+    echo "Skipping docker push: Not on the main branch."
+fi
