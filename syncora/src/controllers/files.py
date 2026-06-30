0ffed149ddde92a3dfbf6bb20e69d50f2e5729f3
Miguel Antoons <miguel.antoons@gmail.com>
2026-06-30 21:13:08 +0200

    chore: streamlining files controller return types to be bytes. Response transformation is done in routes file

--- syncora/src/controllers/files.py
+++ syncora/src/controllers/files.py
@@ -1,7 +1,5 @@
 import io
-from typing import TYPE_CHECKING
 
-from flask import make_response
 from reportlab.pdfgen.canvas import Canvas
 
 from controllers.bill_gen import create_bill
@@ -10,11 +8,8 @@ from models.bills import BillModel
 from models.cnotes import CnoteModel
 from models.customers import CustomerModel
 
-if TYPE_CHECKING:
-    from flask.wrappers import Response
 
-
-def get_customer_file(customer_id: int) -> Response:
+def get_customer_file(customer_id: int) -> bytes:
     customer_info = CustomerModel.get_one(customer_id)
 
     pdf_buffer = io.BytesIO()
@@ -35,29 +30,16 @@ def get_customer_file(customer_id: int) -> Response:
     canvas.drawString(100, 610, f"Commentaire: {customer_info.comment}")
     canvas.save()
 
-    response = make_response(pdf_buffer.getvalue())
-    response.headers.set("Content-Type", "application/pdf")
-    response.headers.set(
-        "Content-Disposition", "inline", filename=f"customer_{customer_id}.pdf"
-    )
-    return response
+    return pdf_buffer.getvalue()
 
 
-def get_bill_file(bill_id: str) -> Response:
+def get_bill_file(bill_id: str) -> bytes:
     order_data = BillModel.get_one(bill_id)
     customer_data = CustomerModel.get_one(order_data.customerId)
-    pdf_bytes = create_bill(order_data, customer_data)
-    resp = make_response(pdf_bytes)
-    resp.headers.set("Content-Type", "application/pdf")
-    resp.headers.set("Content-Disposition", "inline", filename=f"bill_{bill_id}.pdf")
-    return resp
+    return create_bill(order_data, customer_data)
 
 
-def get_cnote_file(cnote_id: str) -> Response:
+def get_cnote_file(cnote_id: str) -> bytes:
     order_data = CnoteModel.get_one(cnote_id)
     customer_data = CustomerModel.get_one(order_data.customerId)
-    pdf_bytes = create_cnote(order_data, customer_data)
-    resp = make_response(pdf_bytes)
-    resp.headers.set("Content-Type", "application/pdf")
-    resp.headers.set("Content-Disposition", "inline", filename=f"cnote_{cnote_id}.pdf")
-    return resp
+    return create_cnote(order_data, customer_data)
