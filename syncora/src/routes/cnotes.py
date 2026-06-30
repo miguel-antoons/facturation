231496eaa7638128ef1e5585278083741a9c7245
Miguel Antoons <miguel.antoons@gmail.com>
2026-06-30 20:45:55 +0200

    chore: streamlining cnote controller return types to be dicts. Json transformation is done in routes file

--- syncora/src/routes/cnotes.py
+++ syncora/src/routes/cnotes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Response, request
+from flask import Blueprint, Response, abort, jsonify, request
 
 from controllers import cnotes as ctrl
 
@@ -6,34 +6,46 @@ cnotes = Blueprint("cnotes", __name__)
 
 
 @cnotes.route("/api/cnotes", methods=["GET", "POST"])
-def cnotes_route() -> Response | None:
+def cnotes_route() -> Response:
+    res = None
     if request.method == "GET":
-        return ctrl.get_cnotes()
-    if request.method == "POST":
-        return ctrl.create_cnote(request.json)
-    return None
+        res = ctrl.get_cnotes()
+    elif request.method == "POST":
+        res = ctrl.create_cnote(request.json)
+    else:
+        abort(405, description="Method not allowed for this endpoint")
+    return jsonify(res)
 
 
 @cnotes.route("/api/cnotes/<string:cnote_id>", methods=["GET", "PUT", "DELETE"])
-def cnote_route(cnote_id: str) -> Response | None:
+def cnote_route(cnote_id: str) -> Response:
+    res = None
     if request.method == "GET":
-        return ctrl.get_cnote(cnote_id)
-    if request.method == "PUT":
-        return ctrl.update_cnote(cnote_id, request.json)
-    if request.method == "DELETE":
-        return ctrl.delete_cnote(cnote_id)
-    return None
+        res = ctrl.get_cnote(cnote_id)
+    elif request.method == "PUT":
+        res = ctrl.update_cnote(cnote_id, request.json)
+    elif request.method == "DELETE":
+        res = ctrl.delete_cnote(cnote_id)
+    else:
+        abort(405, description="Method not allowed for this endpoint")
+    return jsonify(res)
 
 
 @cnotes.route("/api/cnotes/sendPeppol/<string:cnote_id>", methods=["POST"])
-def cnote_send_peppol(cnote_id: str) -> Response | None:
+def cnote_send_peppol(cnote_id: str) -> Response:
+    res = None
     if request.method == "POST":
-        return ctrl.send_cnote_peppol(cnote_id)
-    return None
+        res = ctrl.send_cnote_peppol(cnote_id)
+    else:
+        abort(405, description="Method not allowed for this endpoint")
+    return jsonify(res)
 
 
 @cnotes.route("/api/cnotes/sendBillit/<string:cnote_id>", methods=["POST"])
-def cnote_send_billit(cnote_id: str) -> Response | None:
+def cnote_send_billit(cnote_id: str) -> Response:
+    res = None
     if request.method == "POST":
-        return ctrl.send_cnote_billit(cnote_id)
-    return None
+        res = ctrl.send_cnote_billit(cnote_id)
+    else:
+        abort(405, description="Method not allowed for this endpoint")
+    return jsonify(res)
