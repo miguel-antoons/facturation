0fb81256fc7d53d0edd67904da4a068e9050ee93
Miguel Antoons <miguel.antoons@gmail.com>
2026-06-30 20:53:48 +0200

    chore: streamlining customer controller return types to be dicts. Json transformation is done in routes file

--- syncora/src/routes/customers.py
+++ syncora/src/routes/customers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Response, request
+from flask import Blueprint, Response, abort, jsonify, request
 
 from controllers import customers as ctrl
 
@@ -6,20 +6,26 @@ customers = Blueprint("customers", __name__)
 
 
 @customers.route("/api/customers", methods=["GET", "POST"])
-def customers_all() -> Response | None:
+def customers_all() -> Response:
+    res = None
     if request.method == "GET":
-        return ctrl.get_customers()
-    if request.method == "POST":
-        return ctrl.create_customer(request.json)
-    return None
+        res = ctrl.get_customers()
+    elif request.method == "POST":
+        res = ctrl.create_customer(request.json)
+    else:
+        abort(405, description="Method not allowed for this endpoint")
+    return jsonify(res)
 
 
 @customers.route("/api/customers/<int:customer_id>", methods=["GET", "PUT", "DELETE"])
-def customers_id(customer_id: int) -> Response | None:
+def customers_id(customer_id: int) -> Response:
+    res = None
     if request.method == "GET":
-        return ctrl.get_customer(customer_id)
-    if request.method == "PUT":
-        return ctrl.update_customer(customer_id, request.json)
-    if request.method == "DELETE":
-        return ctrl.delete_customer(customer_id)
-    return None
+        res = ctrl.get_customer(customer_id)
+    elif request.method == "PUT":
+        res = ctrl.update_customer(customer_id, request.json)
+    elif request.method == "DELETE":
+        res = ctrl.delete_customer(customer_id)
+    else:
+        abort(405, description="Method not allowed for this endpoint")
+    return jsonify(res)
